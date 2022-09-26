f839c2b5fccae68699d812ebd23a1835f768a604
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-09-26 15:16:39 +0200

    reduce done

--- script.js
+++ script.js
@@ -42,12 +42,6 @@ students.forEach((eachStudent) => {
   }
 });
 
-
-
-
-
-
-
 // * .map() =>  crear un nuevo array con los elementos modificados. SIEMPRE DEBE RETORNAR UN VALOR.
 // map SIEMPRE retorna un array con la misma cantidad de elementos que el arr original
 
@@ -79,95 +73,172 @@ function changeNumbersToStrings(array) {
 
 console.log(changeNumbersToStrings(randomNumbers));
 
-
 // Actividad MAP
 
-
-const fruits = ['pineapple', 'orange', 'mango'];
+const fruits = ["pineapple", "orange", "mango"];
 
 const upperCaseFruits = fruits.map((eachElement) => {
   //your code here
-  console.log(eachElement)
-  return eachElement.toUpperCase()
-})
+  console.log(eachElement);
+  return eachElement.toUpperCase();
+});
 
 // test
-console.log(upperCaseFruits)  
+console.log(upperCaseFruits);
 //Answer should be  ['PINEAPPLE', 'ORANGE', 'MANGO'];
 
-
-// exercise 2: use .map() to create a new array with each city capitalized (only first letter) 
-const cities = ['miami', 'barcelona', 'madrid', 'amsterdam']
-
+// exercise 2: use .map() to create a new array with each city capitalized (only first letter)
+const cities = ["miami", "barcelona", "madrid", "amsterdam"];
 
 let capitalizedCities = cities.map((eachCity) => {
-
   // return eachCity[0].toUpperCase() + eachCity.slice(1)
 
-  let firstLetter = eachCity[0].toUpperCase() 
-  // let firstLetter = eachCity.slice(0,1).toUpperCase() 
-  let restOfWord = eachCity.slice(1)
-  return firstLetter + restOfWord
-})
+  let firstLetter = eachCity[0].toUpperCase();
+  // let firstLetter = eachCity.slice(0,1).toUpperCase()
+  let restOfWord = eachCity.slice(1);
+  return firstLetter + restOfWord;
+});
 
 // Answer should be :
 // [ 'Miami', 'Barcelona', 'Madrid', 'Amsterdam']
-console.log(capitalizedCities)
+console.log(capitalizedCities);
 
-
-// exercise  3: Return only the names of the restaurants 
+// exercise  3: Return only the names of the restaurants
 // Note: Be careful here. 'info' is an object with a key called 'list' which is the array
- 
+
 const info = {
   list: [
-    {name: 'Mirazur', owner : 'Gustavo'},
-    {name: 'Noma', owner : 'Taka'},
-    {name: 'Gaggan', owner : 'Alvaro'},
-    {name: 'Geranium', owner : 'Gustavo'},
-    {name: 'Arpège', owner : 'Luis'},    
- ]  
-}
-
+    { name: "Mirazur", owner: "Gustavo" },
+    { name: "Noma", owner: "Taka" },
+    { name: "Gaggan", owner: "Alvaro" },
+    { name: "Geranium", owner: "Gustavo" },
+    { name: "Arpège", owner: "Luis" },
+  ],
+};
 
 function onlyNames(arr) {
-  console.log(arr)
- //your code here
- let restaurantNames = arr.map((eachRest) => {
-    return eachRest.name
- })
+  console.log(arr);
+  //your code here
+  let restaurantNames = arr.map((eachRest) => {
+    return eachRest.name;
+  });
 
- return restaurantNames
-
+  return restaurantNames;
 }
 
 // test
-const restaurants = onlyNames( info.list )
-console.log(restaurants)
+const restaurants = onlyNames(info.list);
+console.log(restaurants);
 // Answer should be [ 'Mirazur', 'Noma', 'Gaggan', 'Geranium', 'Arpège']
-
 
 // .filter() => nos permite filtrar elementos de un array que pasen una condicion. CREAR UN NUEVO ARRAY.
 // ejemplo. tengo 10 coches. y filtro por color. puedo tener un nuevo array solo con 5 coches.
 
 let cars = [
-  {brand: "Mercedez", year: 2012, color: "blue"},
-  {brand: "Porsche", year: 2000, color: "gray"},
-  {brand: "Seat", year: 2020, color: "black"},
-  {brand: "Volvo", year: 2021, color: "blue"}
-]
+  { brand: "Mercedez", year: 2012, color: "blue" },
+  { brand: "Porsche", year: 2000, color: "gray" },
+  { brand: "Seat", year: 2020, color: "black" },
+  { brand: "Volvo", year: 2021, color: "blue" },
+];
 
 let filteredCars = cars.filter((eachCar) => {
   // no espera que retornemos un element
   // retornemos true (haya que agregarlo) o false (no haya que agregarlo)
   if (eachCar.year > 2010 && eachCar.color === "blue") {
-    return true // agregalo al array
+    return true; // agregalo al array
   } else {
-    return false // ignoralo o no lo agregues al array
+    return false; // ignoralo o no lo agregues al array
   }
 
   // opcional como sintaxis reducida
   // return eachCar.year > 2010 && eachCar.color === "blue" // o true o false
+});
 
-})
+console.log(filteredCars);
 
-console.log(filteredCars)
+// ACTIVITY filter
+
+// Exercise 2: From the given array of people, get the people who are allowed to consume alcoholic beverages (i.e age >= 21).
+const people = [
+  { name: "Candice", age: 25 },
+  { name: "Tammy", age: 30 },
+  { name: "Allen", age: 21 },
+  { name: "Nettie", age: 20 },
+  { name: "Stuart", age: 17 },
+  { name: "Bill", age: 19 },
+];
+
+// note that "adults" is still just a function (just a function of arrow type)
+const adults = (arr) => {
+  //your code here
+  //  console.log(arr)
+  let filteredPeople = arr.filter((eachPerson) => {
+    if (eachPerson.age >= 21) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  //  return filteredPeople 
+
+  // * BONUS. solo los nombres *
+  let onlyNames = filteredPeople.map((eachPerson) => {
+    return eachPerson.name;
+  });
+
+  return onlyNames;
+};
+
+let canDrink = adults(people);
+console.log(canDrink);
+//Answer should be
+/*
+[
+  { name: "Candice", age: 25 },
+  { name: "Tammy", age: 30 },
+  { name: "Allen", age: 21 },
+]
+*/
+
+
+// .reduce() ejecuta sobre cada elemento una funcion reductora, y retorna un valor con la reduccion final
+
+
+let someNums = [1, 5, 10, 22];
+
+// sintaxis
+// let totalReduce = array.reduce( callbackToReduce, initialValueAccumulator )
+
+let totalReduce = someNums.reduce((accumulator, eachElement) => {
+  // el accumulador es donde guardaremos la reducción que estamos haciendo
+  console.log("accumulator:", accumulator, "eachElement:", eachElement)
+  return accumulator + eachElement
+}, 0)
+
+console.log("totalReduce", totalReduce) // suma de los elementos del array
+
+
+let profesores = [
+  { name: "Jorge", candy: 20 },
+  { name: "Carolina", candy: 22 },
+  { name: "Iñigo", candy: 14 },
+  { name: "Phantom" }
+]
+
+let allNames = profesores.reduce((acc, eachPerson) => {
+  return acc + eachPerson.name + " "
+}, "Nombres: ")
+
+console.log(allNames) // "Jorge Carolina Iñigo Phantom"
+
+let allCandy = profesores.reduce((acc, eachPerson) => {
+  // return acc + eachPerson.candy
+  if (eachPerson.candy !== undefined) {
+    return acc + eachPerson.candy
+  } else {
+    return acc
+  }
+}, 0)
+
+console.log(allCandy)
